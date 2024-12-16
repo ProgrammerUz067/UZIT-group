74a8d5107c20200376ab7680a78c66b458bdd9b1
Diyor <baxacap@gmail.com>
2024-12-16 15:00:52 +0500

    uzitgroup

---
+++ src/components/Header/HeaderCenter.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { Link } from 'react-router-dom';
+
+export default function HeaderCenter() {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      {/* Header center */}
+      <ul className="flex items-center">
+        <a href="#about">
+          <li>{t('header.links1')}</li>
+        </a>
+        <a href="#team">
+          <li>{t('header.links2')}</li>
+        </a>
+        <a href="#services">
+          <li>{t('header.links3')}</li>
+        </a>
+        <a href="#projects">
+          <li>{t('header.links4')}</li>
+        </a>
+        <a href="#questions">
+          <li>{t('header.links6')}</li>
+        </a>
+        <a href="#contacts">
+          <li>{t('header.links5')}</li>
+        </a>
+      </ul>
+    </div>
+  );
+}
